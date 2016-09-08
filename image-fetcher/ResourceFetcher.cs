fc7ba56cd22ab42f8df21089e50f490fbb0031df
Kevin Ward <antiomiae@gmail.com>
2016-09-08 10:01:16 -0700

    Add ResourceFetcher.cs, which was accidentally left out

---
+++ image-fetcher/ResourceFetcher.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace ImageFetcher
+{
+    struct FileInfo
+    {
+        public string uri;
+        public Stream contents;
+
+        public void save(string name)
+        {
+            using (var fs = File.Create(name))
+            {
+                contents.CopyTo(fs);
+            }
+        }
+    }
+
+    public class ResourceFetcher
+    {
+        private BlockingCollection<FileInfo> _resultsQ = new BlockingCollection<FileInfo>();
+
+        public ResourceFetcher(string[] imgUrls, string basePath)
+        {
+            dispatchDownloaders(imgUrls);
+            saveImages(basePath);
+
+            _resultsQ.Dispose();
+        }
+
+        private void dispatchDownloaders(string[] uris)
+        {
+            var allTasks = new List<Task>();
+
+            foreach (var uri in uris)
+            {
+                // Schedule downloader and append stream to results queue
+                allTasks.Add(Task.Run(() =>
+                {
+                    _resultsQ.Add(new FileInfo
+                    {
+                        uri = uri,
+                        contents = new Downloader(uri).GetStream()
+                    });
+                }));
+            }
+
+            Task.Factory.ContinueWhenAll(allTasks.ToArray(), (_) => {
+                Task.WaitAll(allTasks.ToArray());
+
+                _resultsQ.CompleteAdding();
+            });
+        }
+
+        private void saveImages(string basePath)
+        {
+            if (!Directory.Exists(basePath))
+            {
+                Directory.CreateDirectory(basePath);
+            }
+
+            foreach (var fileInfo in _resultsQ.GetConsumingEnumerable())
+            {
+                var fileUri = new Uri(fileInfo.uri);
+                var path = fileUri.Segments.Last();
+
+                fileInfo.save(Path.Combine(basePath, path));
+            }
+        }
+    }
+}
