8b17883c3dc1660607d492be53e4195e2ae1f90a
kevinw <kevinw@MEC-DEV12>
2016-09-07 17:33:27 -0700

    Add project files.

---
+++ ImageFetcherTests/DownloaderTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using ImageFetcher;
+
+namespace ImageFetcherTests
+{
+    [TestClass]
+    public class DownloaderTests
+    {
+        [TestMethod]
+        public void TestDownloadResource()
+        {
+            var netPath = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/SMPTE_Color_Bars.svg/2000px-SMPTE_Color_Bars.svg.png";
+
+            var downloader = new Downloader(netPath);
+
+            using (Stream imageStream = downloader.GetStream())
+            {
+                Assert.IsNotNull(imageStream);
+                Assert.IsTrue(imageStream.CanRead);
+                Assert.IsTrue(imageStream.Length > 0);
+            }
+        }
+    }
+}
---
+++ image-fetcher/Downloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace ImageFetcher
+{
+    public class Downloader
+    {
+        private string _path;
+
+        public Downloader(string path)
+        {
+            _path = path;
+        }
+
+        public Stream GetStream()
+        {
+            var req = WebRequest.Create(_path);
+            req.Proxy = null;
+
+            using (var res = (HttpWebResponse) req.GetResponse())
+            using (var responseStream = res.GetResponseStream())
+            {
+                MemoryStream memoryStream = new MemoryStream();
+
+                responseStream.CopyTo(memoryStream);
+
+                // Reset seek position for caller
+                memoryStream.Position = 0;
+
+                return memoryStream;
+            }
+        }
+    }
+}
